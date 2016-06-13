a6d2434e22ca47ace4aaa474542c002f7636a0fe
Nathan Henrie <nate@n8henrie.com>
2016-06-13 14:37:40 -0600

    Add missing commas

--- setup.py
+++ setup.py
@@ -42,9 +42,9 @@ setup(
     classifiers=[
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3"
-        "Programming Language :: Python :: 3.4"
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: Implementation :: PyPy"
     ],
     extras_require={
