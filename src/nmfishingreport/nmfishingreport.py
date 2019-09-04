1f875853e721f9a89f1f79fdfe32e73dda25488a
Nathan Henrie <nate@n8henrie.com>
2019-09-03 21:53:33 -0600

    Run black

--- src/nmfishingreport/nmfishingreport.py
+++ src/nmfishingreport/nmfishingreport.py
@@ -32,7 +32,7 @@ def get_current_report(url):
 
 def get_current_date(soup):
     # ":" Gets in the way of strptime parzing the %z
-    date_str = soup.find('span', class_='updated').text
+    date_str = soup.find("span", class_="updated").text
     date_str_clean = date_str.replace(":", "").strip()
     cur_dt = datetime.datetime.strptime(date_str_clean, "%Y-%m-%dT%H%M%S%z")
     cur_date = cur_dt.date()
@@ -43,31 +43,32 @@ def main(config_file="config.ini"):
     url = "http://www.wildlife.state.nm.us/fishing/weekly-report"
     config = ConfigParser()
     config.read(config_file)
-    fishing_config = config['FISHING_REPORT']
+    fishing_config = config["FISHING_REPORT"]
 
     logging.basicConfig(
         level=logging.WARNING,
-        format=('%(asctime)s %(name)-12s %(lineno)d %(levelname)-8s '
-                '%(message)s'),
-        datefmt='%%Y-%%m-%%d %%H:%%M:%%S',
-        filename=fishing_config.get('logfile', 'fishing_report.log'),
-        filemode='a'
-        )
+        format=(
+            "%(asctime)s %(name)-12s %(lineno)d %(levelname)-8s " "%(message)s"
+        ),
+        datefmt="%%Y-%%m-%%d %%H:%%M:%%S",
+        filename=fishing_config.get("logfile", "fishing_report.log"),
+        filemode="a",
+    )
 
-    logger_name = str(__file__) + " :: " + str(__name__)
+    logger_name = f'{globals().get("__file__", "")} :: {__name__}'
     logger = logging.getLogger(logger_name)
 
     # File where results for fav_fishing_spots written as text
-    outfile = fishing_config.get('outfile')
+    outfile = fishing_config.get("outfile")
 
-    db = fishing_config.get('db', "fishing_reports.db")
+    db = fishing_config.get("db", "fishing_reports.db")
 
-    fav_fishing_spots = split_conf_str(fishing_config.get('fav_spots'))
+    fav_fishing_spots = split_conf_str(fishing_config.get("fav_spots"))
 
-    if config.has_section('NOTIFY'):
-        notify_config = config['NOTIFY']
-        notify_words = split_conf_str(notify_config.get('notify_words'))
-        notify_script = notify_config.get('notify_script')
+    if config.has_section("NOTIFY"):
+        notify_config = config["NOTIFY"]
+        notify_words = split_conf_str(notify_config.get("notify_words"))
+        notify_script = notify_config.get("notify_script")
     else:
         notify_words = []
 
@@ -90,9 +91,10 @@ def main(config_file="config.ini"):
 
         # If old date is unset or newer data available
         if old_date is None or cur_date > old_date[0]:
-            reports_start = soup.find('h2', text="Statewide Waters")
-            all_spots = reports_start.find_all_next(lambda x: x.name == 'p' and
-                                                    x.next.name == "strong")
+            reports_start = soup.find("h2", text="Statewide Waters")
+            all_spots = reports_start.find_all_next(
+                lambda x: x.name == "p" and x.next.name == "strong"
+            )
 
             spots_dict = {}
             for each_spot in all_spots:
@@ -108,25 +110,27 @@ def main(config_file="config.ini"):
                 conn.execute(cmd, infos)
 
             # Special stuff for favorite spots
-            txt_out = ("## NM fishing report for "
-                       "{:%a, %b %d, %Y}:\n\n").format(cur_date)
+            txt_out = (
+                "## NM fishing report for " "{:%a, %b %d, %Y}:\n\n"
+            ).format(cur_date)
             for fav_spot in fav_fishing_spots:
                 notfound = "Report for {} not found".format(fav_spot)
                 fav_report = spots_dict.get(fav_spot, notfound)
                 txt_out += "- **{}**: {}\n".format(fav_spot, fav_report)
 
-                if any(word.lower() in fav_report.lower()
-                       for word in notify_words):
+                if any(
+                    word.lower() in fav_report.lower() for word in notify_words
+                ):
                     with open(notify_script) as f:
                         notify_func = f.read()
                     exec(notify_func, globals())
                     notify_dict = {
-                            'url': url,
-                            'spot': fav_spot,
-                            'report': fav_report
-                            }
+                        "url": url,
+                        "spot": fav_spot,
+                        "report": fav_report,
+                    }
                     notify(notify_dict, config_file=config_file)  # noqa
 
             if outfile is not None:
-                with open(outfile, 'w', encoding="utf8") as f:
+                with open(outfile, "w", encoding="utf8") as f:
                     f.write(txt_out)
