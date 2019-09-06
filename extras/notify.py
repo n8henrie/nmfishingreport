4b604b3a213a3d821b2392f73f09226b8b27bccf
Nathan Henrie <nate@n8henrie.com>
2019-09-06 09:48:17 -0600

    Format with black

--- extras/notify.py
+++ extras/notify.py
@@ -8,22 +8,24 @@ import urllib.request
 import urllib.parse
 
 
-def notify(notify_dict, config_file='config.ini'):
+def notify(notify_dict, config_file="config.ini"):
     config = ConfigParser()
     config.read(config_file)
-    notify_config = config['NOTIFY']
-    user = notify_config.get('user')
-    api_token = notify_config.get('api_token')
+    notify_config = config["NOTIFY"]
+    user = notify_config.get("user")
+    api_token = notify_config.get("api_token")
 
-    api_url = 'https://api.pushover.net/1/messages.json'
+    api_url = "https://api.pushover.net/1/messages.json"
 
-    payload = urllib.parse.urlencode({
-            'token': api_token,
-            'user': user,
-            'message': notify_dict.get('report'),
-            'title': notify_dict.get('spot'),
-            'url': notify_dict.get('url')
-            })
+    payload = urllib.parse.urlencode(
+        {
+            "token": api_token,
+            "user": user,
+            "message": notify_dict.get("report"),
+            "title": notify_dict.get("spot"),
+            "url": notify_dict.get("url"),
+        }
+    )
 
     with urllib.request.urlopen(api_url, data=payload.encode()) as resp:
         return resp.status == 200
